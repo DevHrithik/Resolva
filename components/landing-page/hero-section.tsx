f9c806a4f5a1cf44653c3f27d3c116b12162082f
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-11-04 21:54:27 +0530

    working on landing page

--- components/landing-page/hero-section.tsx
+++ components/landing-page/hero-section.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "../ui/button";
+import gsap from "gsap";
+
 const Herosection = () => {
+  const leftComponent = useRef(null);
+  const rightComponent = useRef(null);
+
+  useEffect(() => {
+    gsap.from(leftComponent.current, {
+      x: -200,
+      opacity: 0,
+      duration: 1.5,
+      ease: "power3.out",
+      stagger:0
+    });
+    gsap.from(rightComponent.current, {
+      x: 200,
+      opacity: 0,
+      duration: 1.5,
+      ease: "power3.out",
+      stagger:0
+    });
+  }, []);
+
   return (
-    <div className="h-[55vh] md:h-[90vh] flex flex-col md:flex-row py-[30%] justify-between items-center ">
-      <div className="flex flex-col gap-4 text-center md:text-left">
-        <div className="text-[40px] md:text-[80px] font-bold  md:leading-tight">
+    <div className="h-[55vh] md:h-screen flex flex-col md:flex-row justify-between items-center py-[30%] md:py-0">
+      <div
+        ref={leftComponent}
+        className="flex flex-col gap-4 text-center md:text-left"
+      >
+        <div className="text-[40px] md:text-[80px] font-bold md:leading-tight">
           <h1>Solve issues</h1>
           <h1 className="-mt-5">earn rewards.</h1>
         </div>
@@ -15,13 +40,16 @@ const Herosection = () => {
           </span>
         </div>
 
-        <div className="">
+        <div>
           <Button className="bg-[#2f93e0] hover:bg-[#006AB9] text-[16px] font-medium">
             Sign up for free
           </Button>
         </div>
       </div>
-      <div className="hidden md:block w-[400px] h-[400px] bg-gray-200 rounded-lg"></div>
+      <div
+        ref={rightComponent}
+        className="hidden md:block w-[50%] h-[400px] bg-gray-500 rounded-lg"
+      ></div>
     </div>
   );
 };
