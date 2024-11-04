24c078bdde49cd35c0a0547a73e387ad9b3f4828
subhajitorrin <subhajitghosh343@gmail.com>
2024-11-04 17:21:45 +0530

    navbar for desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Navbar from "@/components/navbar";
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <h1>Hello From Hrithiks Issue Branch 2</h1>
     </>
   );
